6d30d1a27a269c0c57b7b2888ff440d51a341a81
mhossain <m.hossain@blackcover.fr>
2021-01-23 20:54:58 +0100

    first commit

---
+++ components/Comment.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="flex flex-col justify-between lg:w-2/4 h-32 border rounded-sm mb-5 mx-auto bg-white"
+  >
+    <div class="h-6 text-gray-700 px-1 text-right">
+      <span class="text-xs text-gray-500 font-medium">{{ comment.date }}</span>
+      <div class="border-b w-11/12 mx-auto border-green-600 pb-2"></div>
+    </div>
+    <div class="px-2">
+      <div>
+        <p class="font-light text-sm text-gray-600">{{ comment.message }}</p>
+      </div>
+    </div>
+    <div
+      class="flex flex-row justify-between px-2 text-xs bg-green-200 text-gray-700"
+    >
+      <span class="font-semibold">{{ comment.author }}</span>
+      <div>
+        <span>likes: {{ comment.likes }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['comment'],
+}
+</script>
---
+++ pages/comments/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <Comment
+      v-for="(comment, index) in comments"
+      :key="index"
+      :comment="comment"
+    />
+  </div>
+</template>
+
+<script>
+import Comment from '~/components/Comment.vue'
+export default {
+  components: { Comment },
+  data() {
+    return {
+      comments: [
+        {
+          message:
+            'Hi guys i am really proud to join this group and be able to meet new bengladeshis friends !',
+          author: 'Shahid',
+          likes: 23,
+          date: '12-11-2020',
+        },
+        {
+          message:
+            'Hae duae provinciae bello quondam piratico catervis mixtae praedonum a Servilio pro consule missae sub iugum factae sunt vectigales.',
+          author: 'Nadim',
+          likes: 11,
+          date: '12-11-2020',
+        },
+        {
+          message:
+            'Et hae quidem regiones velut in prominenti terrarum lingua positae ob orbe eoo monte Amano disparantur.',
+          author: 'Dini',
+          likes: 7,
+          date: '12-11-2020',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
